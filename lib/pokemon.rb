161bc9863439f078fb4872ec85753299af925071
teerawat prarom <teewat@yahoo.com>
2021-12-09 00:20:34 +1100

    completed assignment

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+    attr_accessor :name, :type, :db 
+    attr_reader :id 
+
+    def initialize(id: , name:, type:, db:)
+        @id = id 
+        @name = name
+        @type = type
+        @db = db 
+    end 
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+                INSERT INTO pokemon (name, type)
+                VALUES (?,?)
+                SQL
+                db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+                SELECT * FROM pokemon 
+                WHERE id = ?
+                SQL
+                db.execute(sql, id).map do |row|
+                    self.new(id: row[0],name: row[1], type: row[2], db: db)
+                end.first
+    end
+
+
 end
